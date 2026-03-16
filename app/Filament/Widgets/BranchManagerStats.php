517199a3b7822ea9f724b128d3cbc9aa081f0613
mohamed-alofq <alofqksa@gmail.com>
2026-03-16 06:57:27 +0300

    feat: make BranchManagerStats cards clickable to filtered lists

--- app/Filament/Widgets/BranchManagerStats.php
+++ app/Filament/Widgets/BranchManagerStats.php
@@ -58,22 +58,35 @@ class BranchManagerStats extends BaseWidget
             Stat::make(__('شحنات واردة قيد الانتظار'), $pendingIncoming)
                 ->description(__('في الطريق أو قيد التجهيز للمستودع'))
                 ->descriptionIcon('heroicon-m-arrow-down-tray')
-                ->color('warning'),
+                ->color('warning')
+                ->url(route('filament.admin.resources.stock-transfers.index', [
+                    'tableFilters[internal_status][values][0]' => StockTransfer::STATUS_NEW,
+                    'tableFilters[internal_status][values][1]' => StockTransfer::STATUS_SHIPPED,
+                ])),
 
             Stat::make(__('شحنات صادرة قيد الانتظار'), $pendingOutgoing)
                 ->description(__('تنتظر الشحن'))
                 ->descriptionIcon('heroicon-m-arrow-up-tray')
-                ->color('gray'),
+                ->color('gray')
+                ->url(route('filament.admin.resources.stock-transfers.index', [
+                    'tableFilters[internal_status][value]' => StockTransfer::STATUS_NEW,
+                ])),
 
             Stat::make(__('شحنات متأخرة جداً'), $delayedTransfers)
                 ->description(__('تجاوزت اسبوع - يرجى المتابعة'))
                 ->descriptionIcon('heroicon-m-exclamation-triangle')
-                ->color($delayedTransfers > 0 ? 'danger' : 'success'),
+                ->color($delayedTransfers > 0 ? 'danger' : 'success')
+                ->url(route('filament.admin.resources.stock-transfers.index', [
+                    'tableFilters[is_overdue][value]' => '1',
+                ])),
 
             Stat::make(__('شحنات مكتملة (هذا الشهر)'), $completedThisMonth)
                 ->description(__('شحنات تم إغلاقها بنجاح'))
                 ->descriptionIcon('heroicon-m-check-badge')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.admin.resources.stock-transfers.index', [
+                    'tableFilters[internal_status][value]' => StockTransfer::STATUS_COMPLETED,
+                ])),
         ];
     }
 }
